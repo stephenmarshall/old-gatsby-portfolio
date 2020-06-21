d80630978dc1e866774e6fe2291f884e1b82b6e5
Stephen Marshall <stephenmarshall@iMac.local>
2020-06-21 08:48:42 +0100

    Initial commit

---
+++ src/content/hero.md
@@ -0,0 +1,9 @@
+---
+greetings: "Heey"
+emoji: "👋"
+title: "I'm Stephen Marshall"
+subtitlePrefix: "I build and design "
+subtitleHighlight: "things for the web."
+---
+
+Product Manager and Full Stack Developer. Based in London.
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Hero from "../components/hero"
+
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <Hero content={data.hero.edges[0].node} />
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  {
+    hero: allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            greetings
+            emoji
+            subtitlePrefix
+            subtitleHighlight
+          }
+          rawMarkdownBody
+        }
+      }
+    }
+  }
+`
